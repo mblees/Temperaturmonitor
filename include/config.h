76b088daa4800318d6a27be51435eff7126c78fd
mblees <mariusblees@gmail.com>
2025-07-25 16:05:43 +0200

    1.0.0dev2: removed unused functionality

--- include/config.h
+++ include/config.h
@@ -7,14 +7,6 @@
 #define SERIAL_BAUDRATE 115200
 #define WAIT_FOR_SERIAL true
 
-// MQTT Configuration
-#define ENABLE_MQTT true
-#define MQTT_IP_1 192 // IP address of the MQTT broker, separated by dots
-#define MQTT_IP_2 168
-#define MQTT_IP_3 29
-#define MQTT_IP_4 108
-const static String mqtt_client_name = "Type_of_the_Controller"; // W5100S-EVB-Pico, Pico2W, etc.
-
 // Ethernet Configuration
 #define ENABLE_ETHERNET false
 
@@ -22,16 +14,5 @@ const static String mqtt_client_name = "Type_of_the_Controller"; // W5100S-EVB-P
 #define ENABLE_WIFI true
 #define WIFI_SSID "EmRoLab-ng" // Replace with your WiFi SSID
 #define WIFI_PASSWORD "#HTWdS5307%_" // Replace with your WiFi password
-
-// OTA Configuration
-#define ENABLE_OTA true
-
-// LED Configuration
-#define LED_AMOUNT 30 
-#define LED_DATA_PIN 8
-#define LED_BRIGHTNESS 255  // maximum brightness can draw high current
-
-// Servo Configuration
-#define SERVO_DATA_PIN 9
 
 #endif // CONFIG_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,23 @@
 #include "main.h"
 
-void network_keep_alive(void);
 void setup_network(void);
 
 void setup()
 {
   setup_network();
-
-  // --- Example code ---
-  mqtt_subscribe("hardware/W5100S-Example/topic");
-
-  led_init();
-  led_fill_color(0, 120, 255);
-
-  servo_init();
-
   init_timer(TIMER0, 10000);
-
-  pinMode(26, INPUT);
-  // --- Example code end ---
 }
 
 void loop()
 {
-  network_keep_alive();
-
-  // --- Example code ---
-  led_rainbow(1);
-
-  static int direction = 0;
-  static int count = 0;
   if (timer0_flag)
   {
-    timer0_flag = false;
-    count++;
-    String message = "Timer 0 callback count: " + String(count);
-    mqtt_publish("hardware/W5100S-Example/topic", message.c_str());
-    direction = !direction;
+    Serial.println("Timer 0 triggered");
   }
-
-  if (direction)
-    servo_rotate(0);
-  else
-    servo_rotate(180);
-  // --- Example code end ---
 }
 
 /*
- * @brief Keeps MQTT, Ethernet and OTA alive
- */
-void network_keep_alive()
-{
-#if ENABLE_ETHERNET && ENABLE_MQTT
-  if (!is_mqtt_connected()) // losing MQTT connection means no Internet or MQTT server down
-  {
-    Serial.println("Ethernet disconnected -> Rebooting the board");
-    rp2040.reboot(); // couldn't properly restart the ethernet connection, so reboot required
-  }
-  else
-  {
-    mqtt_loop();
-#if ENABLE_OTA
-    handle_ota();
-#endif
-  }
-#endif
-}
-
-/*
- * @brief setup MQTT, Ethernet and OTA
+ * @brief setup WiFi and Serial connection
  */
 void setup_network()
 {
@@ -82,20 +31,5 @@ void setup_network()
 #if ENABLE_WIFI
   init_wifi(); // Initialize WiFi
   connect_wifi(); // Connect to WiFi
-#endif
-
-#if ENABLE_ETHERNET
-  const IPAddress static_ip(0, 0, 0, 0); // leave 0.0.0.0 for DHCP
-  init_ethernet(static_ip);
-  wait_for_ethernet(); // wait for Ethernet connection
-#endif
-
-#if ENABLE_MQTT
-  mqtt_connect();  // connect to MQTT broker
-  wait_for_mqtt(); // wait for MQTT connection
-#endif
-
-#if ENABLE_OTA
-  init_ota(); // initialize OTA
 #endif
 }
